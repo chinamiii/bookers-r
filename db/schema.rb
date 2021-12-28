52fb6ce022e3a13689961c24fa251c0760e664c4
chinamiii <chinami.2929@icloud.com>
2021-12-28 03:47:59 +0000

    [book model]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_123521) do
+ActiveRecord::Schema.define(version: 2021_12_28_034217) do
+
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "body"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
