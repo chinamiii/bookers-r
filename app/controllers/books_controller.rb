429bcd271c8ea6c7b4aff1a83b608d30202daf97
chinamiii <chinami.2929@icloud.com>
2021-12-30 12:40:16 +0000

    [create book]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,35 @@
+class BooksController < ApplicationController
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+    @books = Book.all
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root to: 'homes#top'
   get '/home/about' => 'homes#about', as: 'about'
   resources :users, only: [:show, :edit, :update, :index]
+  resources :books, only: [:create, :edit, :update, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
