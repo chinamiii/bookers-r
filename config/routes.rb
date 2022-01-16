c064576697ccdd54e86e449fae5b453bb2770502
chinamiii <chinami.2929@icloud.com>
2022-01-16 13:59:01 +0000

    [destroy]

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about' => 'homes#about', as: 'about'
   resources :users, only: [:show, :edit, :update, :index]
-  resources :books, only: [:create, :edit, :update, :index, :show]
+  resources :books, only: [:create, :edit, :update, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
